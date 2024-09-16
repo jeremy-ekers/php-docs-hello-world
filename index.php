d7cb0031e0e0f2b99ff1996e6388a4651532d5c1
Jeremy <jeremy.ekers@forsbergpnt.com>
2024-09-16 13:22:53 +0100

    add proper html wrapping

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<!-- Time-stamp: <13:22:30 Monday 16 September 2024 dJeremy> -->
+<html lang="en">
+  <head>
+    <title>Hello, world</title>
+  </head>
+
+  <body>
+    <?php
 
 echo "Hello World!";
+
+    ?>
+  </body>
+</html>
